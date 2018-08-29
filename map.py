d4b485b355aab6331e36b13721444391b1b50bc2
Paritosh Wadhavane <paritoshwadhavane@gmail.com>
2018-08-29 12:38:45 +0530

    added markers to map

--- map.py
+++ map.py
@@ -1,7 +1,9 @@
 import folium
 
 map = folium.Map(location=[19.118413, 73.030031], zoom_start=6)
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[19.025414, 73.025255], popup="Wonders Park", icon=folium.Icon(color="blue")))
+map.add_child(fg)
 map.save("Map.html")
-
 
 
