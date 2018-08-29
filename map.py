4b0ff542c87c2d11f83431a0f5d65564de5df5a3
Paritosh Wadhavane <paritoshwadhavane@gmail.com>
2018-08-29 16:26:49 +0530

    icon colors based on airport type

--- map.py
+++ map.py
@@ -7,11 +7,26 @@ lat = list(data["latitude_deg"])
 lon = list(data["longitude_deg"])
 name = list(data["name"])
 wiki = list(data["wikipedia_link"])
+typ = list(data["type"])
+
+def color_producer(typ):
+    if str(typ) == 'large_airport':
+        return "black"
+    elif str(typ) == 'medium_airport':
+        return "gray"
+    elif str(typ) == 'small_airport':
+        return "lightgray"
+    elif str(typ) == 'closed':
+        return "white"
+    else:
+        return "darkpurple"
+
 
 map = folium.Map(location=[19.118413, 73.030031], zoom_start=6)
 fg = folium.FeatureGroup(name="My Map")
-for lt, ln, nm, wk in zip(lat, lon, name, wiki):
+for lt, ln, nm, wk, ty in zip(lat, lon, name, wiki, typ):
     fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup('<a href={wk} target="_blank"> {nm} </a>'
-                                                                     .format(wk=wk, nm=nm)), icon=folium.Icon(color="blue")))
+                                                                     .format(wk=wk, nm=nm)) if str(wk) != 'nan' else nm,
+                               icon=folium.Icon(color=color_producer(ty), icon='flag')))
 map.add_child(fg)
 map.save("Map.html")
