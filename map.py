8e4da200845d26eba91883d5d309cd76c5f297ca
Paritosh Wadhavane <paritoshwadhavane@gmail.com>
2018-08-29 14:29:02 +0530

    linked the popup name to wikipedia

--- map.py
+++ map.py
@@ -6,10 +6,12 @@ data = pandas.read_csv("airports-in-india.csv")
 lat = list(data["latitude_deg"])
 lon = list(data["longitude_deg"])
 name = list(data["name"])
+wiki = list(data["wikipedia_link"])
 
 map = folium.Map(location=[19.118413, 73.030031], zoom_start=6)
 fg = folium.FeatureGroup(name="My Map")
-for lt, ln, nm in zip(lat, lon, name):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(nm), icon=folium.Icon(color="blue")))
+for lt, ln, nm, wk in zip(lat, lon, name, wiki):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup('<a href={wk} target="_blank"> {nm} </a>'
+                                                                     .format(wk=wk, nm=nm)), icon=folium.Icon(color="blue")))
 map.add_child(fg)
 map.save("Map.html")
