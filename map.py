1f25037b0ef6115063a923324aa608b484ce6ab1
Paritosh Wadhavane <paritoshwadhavane@gmail.com>
2018-08-29 13:54:08 +0530

    added dataset file, marked all the airports with name popups

--- map.py
+++ map.py
@@ -1,9 +1,15 @@
 import folium
+import pandas
+
+data = pandas.read_csv("airports-in-india.csv")
+
+lat = list(data["latitude_deg"])
+lon = list(data["longitude_deg"])
+name = list(data["name"])
 
 map = folium.Map(location=[19.118413, 73.030031], zoom_start=6)
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[19.025414, 73.025255], popup="Wonders Park", icon=folium.Icon(color="blue")))
+for lt, ln, nm in zip(lat, lon, name):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(nm), icon=folium.Icon(color="blue")))
 map.add_child(fg)
 map.save("Map.html")
-
-
